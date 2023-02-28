aa6f4d04a5e3b75815594f65d3636b34fb84384d
Arthur Rambo <84105405+RamboRthr@users.noreply.github.com>
2023-02-28 16:30:37 -0300

    endpoints done

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import ClassroomViewSet, StudentViewSet, ProfessorViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'classrooms', ClassroomViewSet)
+router.register(r'students', StudentViewSet)
+router.register(r'professors', ProfessorViewSet)
 
 urlpatterns = [
-    path('classroom/post', views.insertClassroom, name='createClassroom'),
-    path('classroom/getAll', views.getClassrooms, name='getAllClassrooms'),
-    path('classroom/delete/<str:pk>', views.deleteClassroom, name='deleteClassroom')
+    #path('classroom/post', views.insertClassroom, name='createClassroom'),
+    #path('classroom/getAll', views.getClassrooms, name='getAllClassrooms'),
+    #path('classroom/delete/<str:pk>', views.deleteClassroom, name='deleteClassroom')
+
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -7,29 +7,45 @@ from rest_framework import status
 from .models import Student, Classroom, Professor
 from .serializers import StudentSerializer, ClassroomSerializer, ProfessorSerializer
 
-@api_view(['GET'])
-def getClassrooms(request):
-    classrooms = Classroom.objects.all()
-    classrooms_serializer = ClassroomSerializer(classrooms, many=True)
-    return JsonResponse(classrooms_serializer.data, safe=False)
 
-@api_view(['POST'])
-def insertClassroom(request):
-    classroom_data = Classroom.objects.create()
-    classroom_data.nbr_students = 0
-    classroom_data.math_mean_note = 0
-    classroom_data.german_mean_note = 0
-    classroom_data.portuguese_mean_note = 0
-    classroom_data.professors.set([]) 
-    classroom_serializer = ClassroomSerializer(data=classroom_data)
-    if classroom_serializer.is_valid():
-        classroom_serializer.save()
-        return JsonResponse(classroom_serializer.data, status=status.HTTP_201_CREATED) 
-    return JsonResponse(classroom_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from rest_framework import viewsets
 
-@api_view(['DELETE'])
-def deleteClassroom(request, pk):
-    classroom = Classroom.objects.get(id=pk)
-    if request.method == 'DELETE':
-        classroom.delete()
-        return JsonResponse({'message': 'Classroom was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+class ClassroomViewSet(viewsets.ModelViewSet):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+
+
+#@api_view(['GET'])
+#def getClassrooms(request):
+#    classrooms = Classroom.objects.all()
+#    classrooms_serializer = ClassroomSerializer(classrooms, many=True)
+#    return JsonResponse(classrooms_serializer.data, safe=False)
+#
+#@api_view(['POST'])
+#def insertClassroom(request):
+#    classroom_data = Classroom.objects.create()
+#    classroom_data.nbr_students = 0
+#    classroom_data.math_mean_note = 0
+#    classroom_data.german_mean_note = 0
+#    classroom_data.portuguese_mean_note = 0
+#    classroom_data.professors.set([]) 
+#    classroom_serializer = ClassroomSerializer(data=classroom_data)
+#    if classroom_serializer.is_valid():
+#        classroom_serializer.save()
+#        return JsonResponse(classroom_serializer.data, status=status.HTTP_201_CREATED) 
+#    return JsonResponse(classroom_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#@api_view(['DELETE'])
+#def deleteClassroom(request, pk):
+#    classroom = Classroom.objects.get(id=pk)
+#    if request.method == 'DELETE':
+#        classroom.delete()
+#        return JsonResponse({'message': 'Classroom was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
